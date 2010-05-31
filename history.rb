c9d762ca78674ca1bab9ffdd22afb8033257d6a1
Chris <kidko@kidkonia.com>
2010-05-31 18:50:08 -0400

    Added a map test to the history.rb file, and started carving mountains out. Makes a variable-length cross at this point; the in-betweens still haven't been implemented

--- history.rb
+++ history.rb
@@ -1,2 +1,7 @@
 require "lib/creature"
 require "lib/map"
+
+# For now, just test the map maker
+
+$map = generate_map
+print_map $map
--- lib/map.rb
+++ lib/map.rb
@@ -1,21 +1,53 @@
 =begin
-Map data: 12x12 array of cells. Each cell is a square of land, one of 3 types:
+Map data: 10x20 array of cells. Each cell is a square of land, one of 3 types:
 	0 -> plains
 	1 -> mountains
 	2 -> river/water
 =end
 
 def generate_map
-	map = Array.new(12)
-	map.map { Array.new(12).each { |cell| 0 } } # Initialize with all plains
+	map = [ [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ], ]
+	mountains = 0.0
+	mount_threshold = (((rand % 25) * 25) + 5) / 100.0
+	while (mountains / 200) < mount_threshold
+		th = rand(4) + 1; bh = rand(4) + 1; lw = rand(4) + 1; rw = rand(4) + 1
+		centerx = rand(10); centery = rand(10);
+		while map[centery][centerx] == 1
+			centerx = rand(20); centery = rand(10); # No repeat centers
+		end
+		
+		map[centery][centerx] = 1 # Set the center
+		mountains += 1
+		((centerx)..(centerx-th)).each { |i|
+			next if i < 0; map[centery][i] = 1; mountains += 1
+		}
+		((centerx)..(centerx+bh)).each { |i|
+			next if i >= 20; map[centery][i] = 1; mountains += 1
+		}
+		((centery)..(centery-lw)).each { |i|
+			next if i < 0; map[i][centerx] = 1; mountains += 1
+		}
+		((centery)..(centery+rw)).each { |i|
+			next if i >= 10; map[i][centerx] = 1; mountains += 1
+		}
+	end
 	return map
 end
 
 def print_map arr
 	letters = '.^~'.split('')
-	arr.each { |row|
-		arr.each { |col|
-			print letters[col]
+	arr.each { |col|
+		col.each { |row|
+			print letters[row]
 		}
 		print "\n"
 	}
