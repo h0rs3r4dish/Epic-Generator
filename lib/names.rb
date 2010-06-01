3c7ff18999f95b30931647cd0a551edb84f71476
Chris <kidko@kidkonia.com>
2010-05-31 20:05:19 -0400

    The base name generator works\!

--- lib/names.rb
+++ lib/names.rb
@@ -1,21 +1,50 @@
 module Name
 
-	public
+	@relations = Hash.new
+	@startpairs = Array.new
+	
+	class << self
+	
+		def init
+			namelist = IO.readlines('data/names.txt').map { |l| l.strip.downcase }
+			namelist.each { |name|
+				loc = 0
+				@startpairs.push( name[loc] + name[loc+1] )
+				while name.length > loc+3
+					firstpair = name[loc] + name[loc+1]
+					secondpair = name[loc+2]
+					secondpair += name[loc+3] if name.length > loc+4
+					@relations[firstpair] ||= Array.new
+					@relations[firstpair].push secondpair
+					loc += 1
+				end
+			}
+		end
 
-	def generate_base
-
-	end
+		def generate_base min_length=-1
+			min_length = rand(8)+3 if min_length == -1
+			word = @startpairs[rand(@startpairs.length)]
+			wl = word.length
+			while wl < min_length
+				choices = @relations[word[wl-2]+word[wl-1]]
+				word += choices[rand(choices.length)]
+				wl = word.length
+			end
+			return word.capitalize
+		end
 	
-	def generate_city
+		def generate_city
 	
-	end
+		end
 	
-	def generate_river
+		def generate_river
 	
-	end
+		end
 	
-	def generate_mountain
+		def generate_mountain
 	
+		end
+		
 	end
 
 end
