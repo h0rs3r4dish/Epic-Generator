66cb103fddc7b4eef06aee8c573f6059498f0225
Chris <kidko@kidkonia.com>
2010-05-31 21:31:27 -0400

    Olothontor finished up the names.rb generators

--- lib/names.rb
+++ lib/names.rb
@@ -2,6 +2,9 @@ module Name
 
 	@relations = Hash.new
 	@startpairs = Array.new
+	@city_postfixes = Array.new
+	@river_postfixes = Array.new
+	@mtn_postfixes = Array.new
 	
 	class << self
 	
@@ -19,6 +22,9 @@ module Name
 					loc += 1
 				end
 			}
+			@mnt_postfixes = IO.readlines('data/mnt_suffix.txt').map { |l| l.chomp }
+			@river_postfixes = IO.readlines('data/river_suffix.txt').map { |l| l.chomp }
+			@city_postfixes = IO.readlines('data/city_suffix.txt').map { |l| l.chomp }
 		end
 
 		def generate_base min_length=-1
@@ -34,15 +40,15 @@ module Name
 		end
 	
 		def generate_city
-	
+			return generate_base + @city_postfixes[rand(@city_postfixes.length)]
 		end
 	
 		def generate_river
-	
+			return generate_base + @river_postfixes[rand(@river_postfixes.length)]
 		end
 	
 		def generate_mountain
-	
+			return generate_base + @mtn_postfixes[rand(@mtn_postfixes.length)]
 		end
 		
 	end
