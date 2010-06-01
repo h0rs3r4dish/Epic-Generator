d01086970861c7ec46600793ff30b5533cd9af09
Chris <kidko@kidkonia.com>
2010-06-01 16:29:41 -0400

    Started the actual design of the history sim. Right now it just ticks for a certain number of years (tracked by months) and tracks fake "events" that tick() generates

--- history.rb
+++ history.rb
@@ -1,14 +1,34 @@
 require "lib/creature"
 require "lib/map"
 require "lib/names"
+require "lib/civilizations"
 
 $flag_color = false
+$flag_maxyear = 500
 
-if ARGV.include? '--color' then
-	ARGV.delete('--color')
-	$flag_color = true
-end
+ARGV.each { |arg|
+	case arg
+		when '--color'
+			$flag_color = true
+		when /\-\-year=([0-9]+)/
+			$flag_maxyear = $1.to_i
+	end
+}
 
 $map = Map.generate
 
 Map.display $map
+
+def tick
+	return rand(10)
+end
+
+month = 0
+event_count = 0
+
+while (month / 12) < $flag_maxyear
+	event_count += tick
+	month += 1
+end
+puts "Year: %s" % (month / 12)
+puts "Events: %s" % event_count
