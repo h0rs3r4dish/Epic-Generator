73b7a16d77ca5ad2b2e5dfd4052a80d121077d9c
Chris <kidko@kidkonia.com>
2010-06-01 16:33:38 -0400

    Moved the civilization-related stuff into the Civ module, which is starting to take shape.

--- lib/civilizations.rb
+++ lib/civilizations.rb
@@ -1,22 +1,37 @@
-class Civilization
-	
-	attr_accessor :race, :towns, :attitudes, :armies
+module Civ
 
-	def initialize(race=:human, towns=[], attitudes={}, armies=[])
-		@race = race
-		@towns = towns
-		@attitudes = attitudes
-		@armies = armies
+	@civs = Hash.new
+	
+	class << self
+		def init
+			RACES.each { |race|
+				@civs[race] = Civilization.new(race)
+			}
+		end
+	end
+	
+	class Civilization
+	
+		attr_accessor :race, :towns, :attitudes, :armies
+
+		def initialize(race=:human, towns=[], attitudes={}, armies=[])
+			@race = race
+			@towns = towns
+			@attitudes = attitudes
+			@armies = armies
+		end
+
 	end
 
+
+	class Town
+
+	end
+
+
+	class Army
+
+	end
+	
 end
 
-
-class Town
-
-end
-
-
-class Army
-
-end
